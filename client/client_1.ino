b489098581d96d960edac3a66878ebeb5f4dd84f
Joao Miguel Francisco de Souza <joao.miguel@sereducacional.com>
2025-04-25 12:02:29 -0300

    initial commit

---
+++ client/client_1.ino
@@ -0,0 +1,67 @@
+#include <ESP8266WiFi.h>
+#include <EEPROM.h>
+#define sensor A0
+
+const char* ssid = "ESP32-AP";
+const char* password = "12345678";
+
+const char* server_ip = "192.168.4.1";
+const uint16_t server_port = 80;
+
+int x;
+int sid = 0;
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  EEPROM.begin(1024);
+
+  pinMode(sensor, INPUT);
+   WiFi.begin(ssid, password);
+  Serial.println("Conecting ao ESP32-AP");
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConected");
+
+
+}
+
+void loop() {
+
+
+  while(!client.connect(server_ip, server_port)){
+    Serial.println(".");
+  }
+  Serial.println("Conected to ESP32 Server");
+  
+  x = analogRead(sensor);
+
+  int tmp; 
+  EEPROM.get(0, tmp);
+  Serial.print("TMP: ");
+  Serial.println(tmp);
+
+  if(tmp == 0){
+    client.println(String(0) + " " + String(x));
+    delay(1000);
+    String string_id = client.readStringUntil('\n');
+    sid = string_id.toInt();
+    EEPROM.put(0, sid);
+    EEPROM.commit();
+    Serial.print("SID: ");
+    Serial.println(sid);
+    delay(500);
+    String msg = client.readStringUntil('\n');
+    Serial.println(msg);
+  }
+  else{
+    sid = tmp;
+    client.println(sid + " " + x);
+    String msg = client.readStringUntil('\n');
+    Serial.println(msg);
+  }
+}
---
+++ server/server.ino
@@ -0,0 +1,122 @@
+#include <WiFi.h>
+#include <EEPROM.h>
+#include <map>
+
+const char* ssid_sta = "jmfs";
+const char* password_sta = "jmfs13052022";
+
+const char* ssid_ap = "ESP32-AP";
+const char* password_ap = "12345678";
+
+std::map<int,int> dict;
+
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  EEPROM.begin(512);
+
+  Serial.println("");
+
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(ssid_sta, password_sta);
+  Serial.println("Conecting to extern wifi");
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConected. IP: " + WiFi.localIP().toString());
+
+  bool ap = WiFi.softAP(ssid_ap,password_ap);
+  if(ap){
+    Serial.println("Access point initiated.");
+  }
+  else{
+    Serial.println("Fail to init AP");
+  }
+  
+  delay(1000);
+
+  server.begin();
+}
+
+void loop() {
+  
+  WiFiClient client = server.available();
+
+  if(client){
+    Serial.println("Client connected");
+
+    while(client.connected()){
+
+      if(client.available()){
+        String id = client.readStringUntil(' ');
+        String value = client.readStringUntil('\n');
+        value.trim();
+
+        int sid = id.toInt();
+        Serial.println(sid);
+        int val = value.toInt();
+
+        if(sid == 0){
+          int i = 0;
+          int tmp;
+          do{
+            EEPROM.get(i, tmp);
+            i++;
+          }while(tmp != -1 && i < 512);
+          EEPROM.put(i, i);
+          EEPROM.commit();
+
+          sid = i;
+          Serial.print("ID: ");
+          Serial.println(sid);
+          client.println(sid);
+
+          if(value.length()>0){
+            if(val == 0 && value != "0"){
+              Serial.println("Invalid value");
+            }
+            else{
+              Serial.print("Value: ");
+              Serial.println(val);
+              client.println("Value received succesfully");
+              dict[sid] = val;
+            }
+          }
+          else{
+            Serial.println("Empty value");
+          }
+        }
+        else{
+          int s; EEPROM.get(sid, s);
+          Serial.print("ID: ");
+          Serial.println(s);
+          if(s == sid){
+            if(value.length()>0){
+              if(val == 0 && value != "0"){
+                Serial.println("Invalid value");
+              }
+              else{
+                Serial.print("Value: ");
+                Serial.println(val);
+                client.println("Value received succesfully");
+                dict[sid] = val;
+              }
+            }
+            else{
+              Serial.println("Empty value");
+            }
+          }
+        }
+
+      }
+    }
+    //client.stop();
+    //Serial.println("Client disconnected");
+  }
+  
+}

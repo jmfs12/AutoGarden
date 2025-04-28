55a2c612cea1c569cea2327b1516db379e239759
João Miguel Francisco de Souza <joanomiguel@gmail.com>
2025-04-28 12:38:47 -0300

    client done

--- client_1/client_1.ino
+++ client_1/client_1.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <EEPROM.h>
+
 #define sensor A0
 #define flag 12
 
@@ -11,75 +12,67 @@ const uint16_t server_port = 80;
 
 int x;
 int sid;
-long timer;
 WiFiClient client;
 
 String sendInfo(){
 
   EEPROM.get(0,sid);
-  Serial.print("SID ANTES: ");
-  Serial.println(sid);
+
   if(client.connect(server_ip, server_port)){
-    
-    Serial.println("Conected to ESP32 Server");
     
     x = analogRead(sensor);
 
-      /*int tmp; 
-      EEPROM.get(0, tmp);
-      Serial.print("TMP: ");
-      Serial.println(tmp);*/
-
     if(sid == -1){
+
       client.println(String(0) + " " + String(x));
       delay(1000);
+      
       String string_id = client.readStringUntil('\n');
-      Serial.print("SID RECEBIDO: ");
-      Serial.println(string_id);
       sid = string_id.toInt();
+
       EEPROM.put(0, sid);
       EEPROM.commit();
-      Serial.print("SID: ");
-      Serial.println(sid);
       delay(500);
+
       String msg = client.readStringUntil('\n');
       client.stop();
-      Serial.println("Disconnected");
       return msg;
-    }
-    else{
-      //sid = tmp;
+    } else{
+
       client.println(String(sid) + " " + String(x));
       String msg = client.readStringUntil('\n');
       client.stop();
-      Serial.println("Disconnected");
       return msg;
+
     }
-  }
-  else return "F";
+
+  } else return "F";
 
 }
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
 
   EEPROM.begin(1024);
 
   pinMode(sensor, INPUT);
   pinMode(flag, OUTPUT);
+
   WiFi.begin(ssid, password);
-  Serial.println("Conecting ao ESP32-AP");
+
   while(WiFi.status() != WL_CONNECTED){
+
     delay(500);
-    Serial.print(".");
+
   }
-  Serial.println("\nConected");
+
 
   String f = "";
-  while(f != "T"){
+  while(f.charAt(0) != 'T'){
+
     f = sendInfo();
+
   }
+
   digitalWrite(flag, HIGH);
   
 }

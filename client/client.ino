fca86e2c204d69b6122fd66ff80e3b2c831c73e1
João Miguel Francisco de Souza <joanomiguel@gmail.com>
2025-04-30 10:51:40 -0300

    reset button working

--- client/client.ino
+++ client/client.ino
@@ -10,7 +10,7 @@ const char* ssid = "ESP32-AP";
 const char* password = "12345678";
 
 const char* server_ip = "192.168.4.1";
-const uint16_t server_port = 80;
+const uint16_t server_port = 80; 
 
 int eid;
 const int pos_id = 0;
@@ -23,14 +23,21 @@ unsigned long ledOnTime = 0;
 String conf = ""; 
 
 String sendId(){
+  Serial.println("Função sendId chamada");
 
   if(client.connect(server_ip, server_port)){
+    Serial.println("Conectado ao servidor");
+
     client.println("send");
     EEPROM.get(pos_id, eid);
 
     int value = analogRead(sensor);
+    Serial.print("Valor do sensor: ");
+    Serial.println(value);
 
     if(eid == -1){
+      Serial.println("ID não encontrado na EEPROM, atribuindo novo ID");
+
       client.println("0 " + String(value));
       delay(500);
 
@@ -41,27 +48,37 @@ String sendId(){
       if (eid >= 0 && eid < 511){
         EEPROM.put(0, eid);
         EEPROM.commit();
+        Serial.println("Novo ID gravado na EEPROM");
       }
+
       client.stop();
       return msg;
     }
     else if (eid >= 0 && eid < 511){
+      Serial.println("ID encontrado, enviando dados");
       client.println(String(eid) + " " + String(value));
 
       String msg = client.readStringUntil('\n');
       client.stop();
       return msg;
     }
-    else return "F";
+    else {
+      Serial.println("ID inválido");
+      return "F";
+    }
 
   }
-  else return "F";
-
+  else {
+    Serial.println("Falha na conexão com o servidor");
+    return "F";
+  }
 }
 
 String resetId(){
+  Serial.println("Função resetId chamada");
 
   if (client.connect(server_ip, server_port)){
+    Serial.println("Conectado ao servidor para reset");
 
     client.println("reset");
     EEPROM.get(pos_id, eid);
@@ -74,15 +91,23 @@ String resetId(){
     String msg = client.readStringUntil('\n');
     msg.trim();
     client.stop();
+
+    if (msg == "R") {
+      Serial.println("ID resetado com sucesso");
+    } else {
+      Serial.println("Falha ao resetar ID");
+    }
+    
     return msg;
 
   }
-  return "N";
-
+  else {
+    Serial.println("Falha na conexão com o servidor para reset");
+    return "N";
+  }
 }
 
 void setup() {
-
   Serial.begin(115200);
   delay(1000);
 
@@ -94,37 +119,37 @@ void setup() {
   pinMode(led, OUTPUT);
 
   WiFi.begin(ssid, password);
+  Serial.println("Tentando conectar ao WiFi...");
 
   while(WiFi.status() != WL_CONNECTED){
-
     delay(500);
-
+    Serial.print(".");
   }
+
+  Serial.println("\nConexão WiFi estabelecida");
 
   int i = 0;
   bool f = false;
   for(int i = 0; i < 10; i++){
-
     String msg = sendId();
     f = msg.equals("T");
     if (f){
-
+      Serial.println("Mensagem 'T' recebida, acendendo o LED de status");
       digitalWrite(flag, HIGH);
       delay(2000);
       digitalWrite(flag, LOW);
       break;
-
     }
-
   }
-
 }
 
 void loop() {
-
   int bt = digitalRead(button);
+  Serial.print("Estado do botão: ");
+  Serial.println(bt);
 
   if (bt == LOW && !waiting) {
+    Serial.println("Botão pressionado, aguardando 2 segundos...");
     waiting = true;
     now = millis();
     ledActivated = false;
@@ -132,26 +157,28 @@ void loop() {
 
   if (waiting) {
     if (digitalRead(button) == LOW && millis() - now >= 2000 && !ledActivated) {
+      Serial.println("2 segundos se passaram, acionando o reset...");
       conf = resetId();  // chama aqui antes de acender o LED
 
       if (conf == "R") {
+        Serial.println("Reset realizado, ligando o LED");
         digitalWrite(led, HIGH);
       }
-      
+
       ledOnTime = millis();
       ledActivated = true;
     }
 
     if (ledActivated && millis() - ledOnTime >= 2000) {
+      Serial.println("LED ficou ligado por 2 segundos, apagando...");
       digitalWrite(led, LOW);
-      Serial.println("Resetando");
+      Serial.println("Resetando...");
       waiting = false;
     }
 
     if (digitalRead(button) == HIGH && !ledActivated) {
       waiting = false;
+      Serial.println("Botão liberado, reset cancelado.");
     }
   }
-
-
 }
--- server/server.ino
+++ server/server.ino
@@ -10,68 +10,93 @@ WiFiServer server(80);
 WiFiClient client;
 
 void sendId(){
+  Serial.println("Função sendId chamada");
+  
   int id = client.readStringUntil(' ').toInt();
   String str_value = client.readStringUntil('\n');
   str_value.trim();
   int value = str_value.toInt();
 
+  Serial.print("ID recebido: ");
+  Serial.println(id);
+  Serial.print("Valor recebido: ");
+  Serial.println(value);
+
   if (id == 0){
+    Serial.println("ID é 0, procurando um ID livre na EEPROM...");
 
     int i = 300;
     int tmp;
-    do{
+    do {
       EEPROM.get(i, tmp);
       i++;
-    }while(tmp != -1 && i < 512);
+    } while(tmp != -1 && i < 512);
 
     id = i;
-    EEPROM.put(id,id);
+    EEPROM.put(id, id);
     EEPROM.commit();
+
+    Serial.print("Novo ID atribuído: ");
+    Serial.println(id);
 
     if (value <= 0)
       client.println(String(id) + " F");
     else 
       client.println(String(id) + " T");
 
+    Serial.println("Resposta enviada ao cliente");
   }
-  else if(id > 0 && id < 512){
-    
+  else if (id > 0 && id < 512){
+    Serial.println("ID válido, verificando valor...");
+
     if (value <= 0)
       client.println("F");
     else 
       client.println("T");
 
+    Serial.println("Resposta enviada ao cliente");
   }
 
 }
- 
+
 void resetId(){
+  Serial.println("Função resetId chamada");
 
   int id = client.readStringUntil('\n').toInt();
   int sid;
   EEPROM.get(id, sid);
-  if( sid == id ){
+
+  Serial.print("ID recebido para reset: ");
+  Serial.println(id);
+  Serial.print("ID armazenado na EEPROM: ");
+  Serial.println(sid);
+
+  if(sid == id){
     EEPROM.put(id, -1);
     EEPROM.commit();
     client.println("R");
+    Serial.println("ID resetado com sucesso");
   }
-
-  else{
+  else {
     client.println("N");
+    Serial.println("ID não encontrado, resposta N enviada");
   }
-
 }
 
 void setFunction(){
-  
   String function = client.readStringUntil('\n');
-  if(function.equals("reset")){
+  Serial.print("Função recebida: ");
+  Serial.println(function);
+
+  if(function.charAt(0) == 'r'){
     resetId();
   }
-  else if(function.equals("send")){
+  else if(function.charAt(0) == 's'){
     sendId();
   }
-
+  else {
+    Serial.println("Função desconhecida");
+  }
 }
 
 void setup() {
@@ -79,36 +104,41 @@ void setup() {
   delay(1000);
   EEPROM.begin(512);
 
+  Serial.println("Iniciando conexão Wi-Fi...");
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid_sta, password_sta);
 
   while(WiFi.status() != WL_CONNECTED){
     delay(500);
+    Serial.print(".");
   }
 
-  bool ap = WiFi.softAP(ssid_ap,password_ap);
+  Serial.println("Conexão Wi-Fi estabelecida");
+  bool ap = WiFi.softAP(ssid_ap, password_ap);
+  if (ap) {
+    Serial.println("Modo AP ativo");
+  } else {
+    Serial.println("Falha ao ativar o AP");
+  }
 
   delay(1000);
 
   server.begin();
-  Serial.println("Server criado");
-
+  Serial.println("Servidor iniciado, aguardando conexões...");
 }
 
 void loop() {
   client = server.available();
   if(client){
+    Serial.println("Cliente conectado!");
 
     while(client.connected()){
-
       if(client.available()){
-
         setFunction();
-
       }
-
     }
+
+    Serial.println("Cliente desconectado");
     client.stop();
   }
-
 }

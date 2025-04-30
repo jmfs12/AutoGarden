c7243db90d207873eec618f7de9274d7050a6580
João Miguel Francisco de Souza <joanomiguel@gmail.com>
2025-04-30 10:54:58 -0300

    Add files via upload

--- server/server.ino
+++ server/server.ino
@@ -1,122 +1,144 @@
-#include <WiFi.h>
 #include <EEPROM.h>
-#include <map>
+#include <WiFi.h>
 
 const char* ssid_sta = "jmfs";
 const char* password_sta = "jmfs13052022";
-
 const char* ssid_ap = "ESP32-AP";
 const char* password_ap = "12345678";
 
-std::map<int,int> dict;
-
 WiFiServer server(80);
+WiFiClient client;
+
+void sendId(){
+  Serial.println("Função sendId chamada");
+  
+  int id = client.readStringUntil(' ').toInt();
+  String str_value = client.readStringUntil('\n');
+  str_value.trim();
+  int value = str_value.toInt();
+
+  Serial.print("ID recebido: ");
+  Serial.println(id);
+  Serial.print("Valor recebido: ");
+  Serial.println(value);
+
+  if (id == 0){
+    Serial.println("ID é 0, procurando um ID livre na EEPROM...");
+
+    int i = 300;
+    int tmp;
+    do {
+      EEPROM.get(i, tmp);
+      i++;
+    } while(tmp != -1 && i < 512);
+
+    id = i;
+    EEPROM.put(id, id);
+    EEPROM.commit();
+
+    Serial.print("Novo ID atribuído: ");
+    Serial.println(id);
+
+    if (value <= 0)
+      client.println(String(id) + " F");
+    else 
+      client.println(String(id) + " T");
+
+    Serial.println("Resposta enviada ao cliente");
+  }
+  else if (id > 0 && id < 512){
+    Serial.println("ID válido, verificando valor...");
+
+    if (value <= 0)
+      client.println("F");
+    else 
+      client.println("T");
+
+    Serial.println("Resposta enviada ao cliente");
+  }
+
+}
+
+void resetId(){
+  Serial.println("Função resetId chamada");
+
+  int id = client.readStringUntil('\n').toInt();
+  int sid;
+  EEPROM.get(id, sid);
+
+  Serial.print("ID recebido para reset: ");
+  Serial.println(id);
+  Serial.print("ID armazenado na EEPROM: ");
+  Serial.println(sid);
+
+  if(sid == id){
+    EEPROM.put(id, -1);
+    EEPROM.commit();
+    client.println("R");
+    Serial.println("ID resetado com sucesso");
+  }
+  else {
+    client.println("N");
+    Serial.println("ID não encontrado, resposta N enviada");
+  }
+}
+
+void setFunction(){
+  String function = client.readStringUntil('\n');
+  Serial.print("Função recebida: ");
+  Serial.println(function);
+
+  if(function.charAt(0) == 'r'){
+    resetId();
+  }
+  else if(function.charAt(0) == 's'){
+    sendId();
+  }
+  else {
+    Serial.println("Função desconhecida");
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   EEPROM.begin(512);
 
-  Serial.println("");
-
+  Serial.println("Iniciando conexão Wi-Fi...");
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid_sta, password_sta);
-  Serial.println("Conecting to extern wifi");
 
   while(WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("\nConected. IP: " + WiFi.localIP().toString());
+  Serial.println("Conexão Wi-Fi estabelecida");
+  bool ap = WiFi.softAP(ssid_ap, password_ap);
+  if (ap) {
+    Serial.println("Modo AP ativo");
+  } else {
+    Serial.println("Falha ao ativar o AP");
+  }
 
-  bool ap = WiFi.softAP(ssid_ap,password_ap);
-  if(ap){
-    Serial.println("Access point initiated.");
-  }
-  else{
-    Serial.println("Fail to init AP");
-  }
-  
   delay(1000);
 
   server.begin();
+  Serial.println("Servidor iniciado, aguardando conexões...");
 }
 
 void loop() {
-  
-  WiFiClient client = server.available();
-
+  client = server.available();
   if(client){
-    Serial.println("Client connected");
+    Serial.println("Cliente conectado!");
 
     while(client.connected()){
-
       if(client.available()){
-        String id = client.readStringUntil(' ');
-        String value = client.readStringUntil('\n');
-        value.trim();
-
-        Serial.println("sid antes: " + id);
-        int sid = id.toInt();
-        int val = value.toInt();
-
-        if(sid == 0){
-          int i = 0;
-          int tmp;
-          do{
-            EEPROM.get(i, tmp);
-            i++;
-          }while(tmp != -1 && i < 512);
-          EEPROM.put(i, i);
-          EEPROM.commit();
-
-          sid = i;
-          Serial.print("sid depois: ");
-          Serial.println(sid);
-          client.println(sid);
-
-          if(value.length()>0){
-            if(val == 0 && value != "0"){
-              Serial.println("Invalid value");
-            }
-            else{
-              Serial.print("Value: ");
-              Serial.println(val);
-              client.println("T");
-              dict[sid] = val;
-            }
-          }
-          else{
-            client.println("F");
-          }
-        }
-        else{
-          int s; EEPROM.get(sid, s);
-          Serial.print("ID: ");
-          Serial.println(s);
-          if(s == sid){
-            if(value.length()>0){
-              if(val == 0 && value != "0"){
-                Serial.println("Invalid value");
-              }
-              else{
-                Serial.print("Value: ");
-                Serial.println(val);
-                client.println("T");
-                dict[sid] = val;
-              }
-            }
-            else{
-              client.println("F");
-            }
-          }
-        }
-
+        setFunction();
       }
     }
+
+    Serial.println("Cliente desconectado");
     client.stop();
-    //Serial.println("Client disconnected");
   }
-  
 }
